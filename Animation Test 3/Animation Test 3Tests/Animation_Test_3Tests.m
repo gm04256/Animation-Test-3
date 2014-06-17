529c59c86f579e5878a564ef477a31daddec94dd
Ma Yan <896098846@qq.com>
2014-06-17 15:33:10 +0900

    Project Created

---
+++ Animation Test 3/Animation Test 3Tests/Animation_Test_3Tests.m
@@ -0,0 +1,34 @@
+//
+//  Animation_Test_3Tests.m
+//  Animation Test 3Tests
+//
+//  Created by 馬 岩 on 14-6-17.
+//  Copyright (c) 2014年 馬 岩. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Animation_Test_3Tests : XCTestCase
+
+@end
+
+@implementation Animation_Test_3Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
